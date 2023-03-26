5410261f456f8f6cdb89da957a493084da3e2a8e
Mariano Murcia Ortuño <m.murcia@gmail.com>
2023-03-27 00:48:49 +0200

    Version test_al_crear_un_coche_su_velocidad_es_cero

--- TTD Proyecto Coche v2.0/tests/TestCoche.java
+++ TTD Proyecto Coche v2.0/tests/TestCoche.java
@@ -1,12 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestCoche {
 
 	@Test
-	public void test_crear_coche() {
+	public void test_al_crear_un_coche_su_velocidad_es_cero() {
 		Coche nuevoCoche = new Coche();
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 
 }
